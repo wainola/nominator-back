7bd873652f1f1a6c5e7d87882abbb85a1ec802f6
Nicolás Riquelme <nrriquelme@gmail.com>
2019-02-25 23:09:08 -0300

    WIP userhandler

--- models/Base.js
+++ models/Base.js
@@ -1,13 +1,3 @@
-require('dotenv').config();
-const { Client } = require('pg');
-
-const { DATABASE_URL } = process.env;
-
-const conn = new Client({
-  connectionString: DATABASE_URL
-});
-
-conn.connect();
 class Base {
   static async query(queryToExec) {
       const q = await conn.query(queryToExec)
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
+const UserHandler = require('../handlers/UserHandler');
 
 router.get('/test2', (req, resp) => resp.status(200).send({ msg: 'test two' }));
+
+router.post('/register', UserHandler.register);
 
 module.exports = router;
