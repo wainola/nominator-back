17e3cdd44f1f450b2eb58b11dbf2d99f3438dbc2
Nicolás Riquelme <nrriquelme@gmail.com>
2019-02-25 21:56:51 -0300

    user test, handler, model and test

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
+<<<<<<< HEAD
 const UserHandler = require('../handlers/UserHandler');
 
 router.get('/test2', (req, resp) => resp.status(200).send({ msg: 'test two' }));
 
 router.post('/register', UserHandler.register);
 
+=======
+
+router.get('/test2', (req, resp) => resp.status(200).send({ msg: 'test two' }));
+
+>>>>>>> user test, handler, model and test
 module.exports = router;
