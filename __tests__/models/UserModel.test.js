5ffd7cf337cfd919bf4d881262c85b4b612d3b2b
Nicolás Riquelme <nrriquelme@gmail.com>
2019-03-01 19:12:41 -0300

    rebase to master

--- __tests__/models/UserModel.test.js
+++
@@ -1,5 +0,0 @@
-describe('UserModel', () => {
-  it('should return a registered user', async () => {});
-  it('should return a logged user', async () => {});
-  it('should return the new password for the user', async () => {});
-});
--- handlers/UserHandler.js
+++
@@ -1,8 +0,0 @@
-class UserHandler {
-  static async register(request, response) {
-    console.log('db in request', 'database' in request);
-    return response.status(200).send({ data: 'some shit' });
-  }
-}
-
-module.exports = UserHandler;
