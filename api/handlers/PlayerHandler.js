14bd3628331cfad58cc156901c4fb95c50ea84a0
Alfonso Nicolás Moya Chamorro <alfonsonicolasmoyachamorro@MacBook-Pro-de-Alfonso.local>
2019-04-01 13:29:02 -0300

    add: delete

--- api/handlers/PlayerHandler.js
+++ api/handlers/PlayerHandler.js
@@ -5,15 +5,24 @@ const Errors = require('../errors');
 const PlayerModel = require('../models/PlayerModel');
 
 class PlayerHandler {
-  static async add(req, res) {    
+  static async add(req, res) {
+    // Obtenemos conexión desde request.
+    const { conn } = req.database; 
       // TODO:
       // Obtener objeto del body
       // Validar objeto
       // -----> OK:Insertarlo en la base de datos
-      // -----> NOK: Indicar que no pasa la validación y devolver status adecuado
-    const resultOfRegistration = await PlayerModel.add(req.database["conn"], 'asdf');
-    console.log('player test ', resultOfRegistration);
+      // -----> NOK: Indicar que no pasa la validación y devolver estado
+    const result = await PlayerModel.add(conn, 'asdf');
+    console.log('Result ', result);
     return res.status(200).send({ data: 'Si ves este mensaje, es porque le dimos a la DB T.T' });
+  }
+
+  static async delete(req, res) {
+    const { conn } = req.database;
+    const { id } = req.body;
+    const result = await PlayerModel.delete(conn, id);
+    return result > 0 ? res.status(200).send({ data: "OK" }) : res.status(203).send({ data: "NOK" });
   }
 }
 
--- api/models/PlayerModel.js
+++ api/models/PlayerModel.js
@@ -1,15 +1,25 @@
 const Base = require('./Base');
 
 class PlayerModel extends Base {
-  // Aquí, cuando creemos la tabla, deberíamos hacer insert, upd, delete ? 
   static async add(db, data) {
-    await db.query("SELECT NOW() as now", (err, res) => {
+    const { player } = data;
+    await db.query(`INSERT INTO players VALUES ()`, (err, res) => {
       if (err) {
         console.log(err.stack)
       } else {
         console.log(res.rows[0])
       }
     })
+  }
+
+  static async delete(db, data){
+    const { id } = data;
+    await db.query(`DELETE FROM players WHERE id = ${id};`, (err, res) => {
+      if (err)
+        return 0
+      else
+        return 1;
+    });
   }
 }
 
